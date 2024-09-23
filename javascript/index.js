bd9a4750b3d4f7aa4bd8e9e39d3f47ec02f8aa68
Gabriel Alvarez <Gabiista8@gmail.com>
2024-09-23 17:06:28 -0300

    actualizacion 7

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,17 @@
 const newYear = document.getElementById('year')
 newYear.innerHTML = new Date().getFullYear();
+
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+}
+showMenu('nav-toggle','nav-menu')
